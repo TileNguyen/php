48ce84c444eff920adfb4e44b43f55ceb4d65d68
Ngoi <ngoin@ants.vn>
2016-11-06 08:36:19 +0700

    init

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>LEARN PHP</title>
+  </head>
+  <body>
+    <?php
+      $capital = 3;
+      print "test a variable $capital<br>";
+      print "An example with sigle comment!";
+
+      // $channel = <<<_XML_
+      //   <channel>
+      //     <title>What's For Dinner</title>
+      //     <link>http://google.com</link>
+      //   </channel>
+      // _XML_;
+      //
+      // echo <<<END ........ END;
+    ?>
+  </body>
+</html>
